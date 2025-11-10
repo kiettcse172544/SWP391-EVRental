48f165389b031157504cc3e8ee225e14cd8c858e
longND03 <longndse170223@fpt.edu.vn>
2025-11-10 14:03:48 +0700

    fix in vehicle

--- EVRenter/EVRenter_API/obj/Debug/net8.0/EVRenter_API.AssemblyInfo.cs
+++ EVRenter/EVRenter_API/obj/Debug/net8.0/EVRenter_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EVRenter_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e895059d48f8de48b4a43c046b57bd7586419406")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70db0a708e3ae76892cd446ae32997c09f23170b")]
 [assembly: System.Reflection.AssemblyProductAttribute("EVRenter_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EVRenter_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
